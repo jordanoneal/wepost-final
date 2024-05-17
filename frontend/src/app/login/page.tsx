0a13d5f548e849b2f0d80e738eb6ae6f1bbdb4dd
Jordan O'Neal <jordanoneal@Jordans-MBP.localdomain>
2024-05-17 15:02:21 -0600

    login screen

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,7 +1,52 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
 
 export default function Login() {
+  const [isClient, setIsClient] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => { };
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) return null;
+
   return (
-    <div>Login</div>
-  )
+    <div className='w-full h-screen flex justify-center items-center bg-gray-200 p-4'>
+      <form onSubmit={handleLogin} className='bg-white shadow-lg rounded-lg p-6 flex flex-col w-full max-w-sm'>
+        <img
+          src='/Login.png'
+          alt='Logo'
+          className='mx-auto mb-6 cover'
+        />
+        <div className='relative flex items-center mb-4'>
+          <FontAwesomeIcon icon={faUser} size='lg' className='absolute left-3 text-gray-500' />
+          <input
+            value={username}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUsername(e.target.value)}
+            className='w-full pl-10 p-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-green-400'
+            type="text"
+            placeholder='Username'
+          />
+        </div>
+        <div className='relative flex items-center mb-4'>
+          <FontAwesomeIcon icon={faLock} size='lg' className='absolute left-3 text-gray-500' />
+          <input
+            value={password}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+            className='w-full pl-10 p-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-green-400'
+            type="password"
+            placeholder='Password'
+          />
+        </div>
+        <button type='submit' className='p-3 bg-green-500 text-white rounded hover:bg-green-600 transition duration-300 mt-6'>Login</button>
+      </form>
+    </div>
+  );
 }
